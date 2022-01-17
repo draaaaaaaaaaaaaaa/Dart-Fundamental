73cc1c178950cdfc9530623e2309614e2136941d
draaaaaaaaaaaaaaa <moh.daffa.ucui@gmail.com>
2022-01-17 14:54:18 +0700

    scond-comit

---
+++ Function/Const_final.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  final firsName = stdin.readLineSync();
+  final lastName = stdin.readLineSync();
+
+  print('Hello $firsName $lastName');
+ 
+}
---
+++ NullSavety.dart
@@ -0,0 +1,9 @@
+main(List<String> args) {
+  String? favoriteFood = null ;
+  int? age = null;
+}
+
+
+buyMeal(String? favoriteFoods){
+  print('bought a $favoriteFoods');
+}
